466f22cc9b4637100d4e291ca389cc31481f76ef
evalynledezma <evalynledezmacode@gmail.com>
2020-10-15 16:13:13 -0600

    react constructor overview

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -2,6 +2,12 @@ import React, { Component} from "react";
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
+    constructor() {
+        super();
+
+        console.log("Portfolio container has rendered")
+    }
+    
     render() {
         return (
             <div>
@@ -10,4 +16,4 @@ export default class PortfolioContainer extends Component {
             </div>
         );
     }
-}
+} 
