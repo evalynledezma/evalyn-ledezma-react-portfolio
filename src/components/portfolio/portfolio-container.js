0f0871387628705cda37fcc901eb7e0b7b13466c
evalynledezma <evalynledezmacode@gmail.com>
2020-10-15 16:29:23 -0600

    custom functions in react adn using map to loop over data

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -7,12 +7,22 @@ export default class PortfolioContainer extends Component {
 
         console.log("Portfolio container has rendered")
     }
-    
+
+    PortfolioItems() {
+        const data = [" "]
+
+        return data.map(item => {
+            return <PortfolioItem/>;
+        })
+    }
+
     render() {
         return (
             <div>
                 <h2>Portfolio items go here...</h2>
-                <PortfolioItem/>
+                
+                {this.PortfolioItems}
+
             </div>
         );
     }
