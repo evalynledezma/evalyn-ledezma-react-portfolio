0c6d807bcb3e5cdb76d17dd0ddac3d3d6f987581
evalynledezma <evalynledezmacode@gmail.com>
2020-10-15 16:05:10 -0600

    class vs functional components

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,10 +1,12 @@
 import React, { Component} from "react";
+import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
     render() {
         return (
             <div>
-             <h2>Portfolio items go here...</h2>
+                <h2>Portfolio items go here...</h2>
+                <PortfolioItem/>
             </div>
         );
     }
