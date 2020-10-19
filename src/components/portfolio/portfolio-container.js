888a1600f576ff4ffc3fe30d1e3614cd21019679
evalynledezma <evalynledezmacode@gmail.com>
2020-10-19 10:34:24 -0600

    intro to working with state in react

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -5,21 +5,27 @@ export default class PortfolioContainer extends Component {
     constructor() {
         super();
 
-        console.log("Portfolio container has rendered")
+        this.state = {
+            pageTitle: "Welcome to my portfolio",
+            data: [
+                {title: "Quip" },
+                {title: "Eventbrite" },
+                {title: "Ministry Safe" },
+                {title: "SwingAway" }
+            ]
+        };
     }
 
     PortfolioItems() {
-        const data = ["Apple", "Banana", "Orange"]
-
-        return data.map(item => {
-            return <PortfolioItem title={item} url="google.com"/>;
-        })
+        return this.state.data.map(item => {
+            return <PortfolioItem title={item.title} url="google.com"/>;
+        });
     }
 
     render() {
         return (
             <div>
-                <h2>Portfolio items go here...</h2>
+                <h2>{this.state.pageTitle}</h2>
                 
                 {this.PortfolioItems()}
 
