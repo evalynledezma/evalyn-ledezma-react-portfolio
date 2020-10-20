a8ad094f4e6e85e34270946c58a34fd52b8121b8
evalynledezma <evalynledezmacode@gmail.com>
2020-10-20 16:32:37 -0600

    nav active link

--- src/components/app.js
+++ src/components/app.js
@@ -6,6 +6,9 @@ import PortfolioContainer from "./portfolio/portfolio-container";
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+
 
 export default class App extends Component {
   render() {
@@ -19,6 +22,9 @@ export default class App extends Component {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/about-me" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/blog" component={Blog} />
+
 
           </Switch>
         </div>
