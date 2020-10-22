452bce521f27308534b56d305d1652b93df3b9e9
evalynledezma <evalynledezmacode@gmail.com>
2020-10-22 12:03:52 -0600

    CSS styles and Building Out the Flexbox Container for the Navigation Component

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,82 +1,76 @@
-import React, { Component} from "react";
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            pageTitle: "Welcome to my portfolio",
-            isLoading: false,
-            data: [
-                {title: "Quip", category: "eCommerce", slug: 'quip'},
-                {title: "Eventbrite", category: "Scheduling", slug: 'eventbrite'},
-                {title: "Ministry Safe", category: "Enterprise", slug: 'ministry-safe'},
-                {title: "SwingAway", category: "eCommerse", slug: 'swingaway'}
-            ]
-        };
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: []
+    };
 
-        this.handleFilter = this.handleFilter.bind(this);
-        this.getPortfolioItems = this.getPortfolioItems.bind(this);
+    this.handleFilter = this.handleFilter.bind(this);
+  }
 
-    }
-    
-    handleFilter(filter) {
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    });
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://jordan.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
         this.setState({
-            data: this.state.data.filter(item => {
-                return item.category === filter;
-            })
+          data: response.data.portfolio_items
         });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return (
+        <PortfolioItem key={item.id} item={item}
+        />
+      );
+    });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
     }
 
-    getPortfolioItems() {
-        axios
-          .get("https://evalynledezma.devcamp.space/portfolio/portfolio_items")
-          .then(response => {
-            this.setState({
-                data: response.data.portfolio_items
-            })
-          })
-          .catch(error => {
-            console.log(error);
-          });
-    }
+    return (
+      <div>
+        <h2>{this.state.pageTitle}</h2>
 
-    PortfolioItems() {
-        return this.state.data.map(item => {
-            return (
-                <PortfolioItem title={item.name} url={item.url} slug={item.id}/>
-            );
-        });
-    }
+        <button onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
 
-    componentDidMount() {
-        this.getPortfolioItems();
-    }
-
-    render() {
-        if (this.state.isLoading) {
-            return <div>Loading...</div>
-        }
-
-        return (
-            <div>
-                <h2>{this.state.pageTitle}</h2>
-                
-                <button onClick={() => this.handleFilter('eCommerse')}>
-                eCommerse
-                </button>
-                <button onClick={() => this.handleFilter('Scheduling')}>
-                Scheduling
-                </button>
-                <button onClick={() => this.handleFilter('Enterprise')}>
-                Enterprise
-                </button>
-
-                {this.PortfolioItems()}
-            </div>
-        );
-    }
+        {this.portfolioItems()}
+      </div>
+    );
+  }
 }
