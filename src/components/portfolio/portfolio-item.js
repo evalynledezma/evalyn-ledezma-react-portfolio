fd9b70d761b26a10c43bee6c0ba5552619ac2359
evalynledezma <evalynledezmacode@gmail.com>
2020-10-22 14:22:36 -0600

    inline styles in react to render background images

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function(props) {
-        //   Data that we'll need:
-    //  - background image: thumb_image_url
-    //  - logp
-    //  - description: description
-    //  - id: id
+  const { id, description, thumb_image_url, logo } = props.item;
+  return (
+    <div className="portfolio-item-wrapper">
+      <div
+        className="portfolio-img-background"
+        style={{
+          backgroundImage: "url(" + thumb_image_url + ")"
+        }}
+      />
 
-    const { id, description, thumb_image_url, logo} = props.item;
-    return (
-        <div className="portfolio-item-wrapper">
-        <img src={thumb_image_url}/>
-        <img src={logo}/>
-        <div>{description}</div>
-            <Link to={`/portfolio/${id}`}>Link</Link>
-        </div>
-    );
+      <img src={logo} />
+      <div>{description}</div>
+      <Link to={`/portfolio/${id}`}>Link</Link>
+    </div>
+  );
 }
