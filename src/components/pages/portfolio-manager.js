5b3852827d8effe36a36cf27a868a061da98171b
evalynledezma <evalynledezmacode@gmail.com>
2020-10-26 11:01:29 -0600

    Building a Secure Class Component for the Portfolio Manager Page

---
+++ src/components/pages/portfolio-manager.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class PortfolioManager extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h1>PortfolioManager</h1>
+        <h1>PortfolioManager</h1>
+      </div>
+    );
+  }
+}
+
+export default PortfolioManager;
