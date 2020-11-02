0932c6d472de09bc647fff8b6f955f684f908fe0
evalynledezma <evalynledezmacode@gmail.com>
2020-11-02 16:06:05 -0700

    Building the Full Blog Creation Workflow

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -30,7 +30,12 @@ export default class BlogForm extends Component {
         { withCredentials: true }
       )
       .then((response) => {
-        this.props.handleSuccessfulFormSubmission(response.data);
+        this.props.handleSuccessfulFormSubmission(response.data.portfolio_blog);
+
+        this.setState({
+          title: "",
+          blog_status: "",
+        });
       })
       .catch((error) => {
         console.log("handleSubmit for blog error", error);
