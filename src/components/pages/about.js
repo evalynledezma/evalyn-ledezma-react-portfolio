efa9da66fe69656c04c6e76ef7a97e013cfe7606
evalynledezma <evalynledezmacode@gmail.com>
2020-11-06 18:36:17 -0700

    Building About Page Styles for React Portfolio

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,46 @@
 import React from "react";
+import profilePicture from "../../../static/assets/images/bio/headshot.jpg";
 
 export default function () {
   return (
-    <div>
-      <h2>About</h2>
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + profilePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+
+      <div className="right-column">
+        Lorem ipsum dolor sit amet, ut viris imperdiet dissentiunt nam, ei alii
+        nostrum phaedrum est. Ancillae mentitum adipiscing ne eum, qui no cibo
+        fuisset constituam. Aeterno impedit verterem vix no, eum ad enim facer
+        nonumes. Vim atqui populo placerat eu. Sed ne sumo posidonium
+        liberavisse, prima option maiorum ei mei. Sed in eros sadipscing, nam
+        purto dolor te. Est ei tantas exerci ornatus, iuvaret scaevola
+        abhorreant pri no, id aeterno lucilius gubergren mei. Ex euismod
+        nominavi qui, ex est erant rationibus dissentiet. Id prima malorum
+        feugiat vel. Ea sed simul efficiendi, ei appetere torquatos eam. No vel
+        aperiri adversarium. Habemus tincidunt usu ex, et sit ubique quodsi
+        platonem, in usu essent neglegentur conclusionemque. Sit in eros
+        noluisse, alia atqui usu ne, pro ad iusto laudem dissentias. Et prima
+        menandri vix, stet constituto eum te, nam erat posse laudem ei. An eos
+        probo munere. Ubique dignissim prodesset vel ex, sit ullum laudem in.
+        Verear efficiendi vix an, delenit nonumes duo ut. Nostro fabulas
+        accommodare pro ad. Sanctus nominavi suscipit nam ad, sed quod utinam
+        electram ut. Qui ex mundi delicatissimi, ne augue dicit appetere eam, et
+        cum aeque explicari. Cum at elitr vituperata liberavisse, cu duo
+        eleifend evertitur. In vix nihil admodum conclusionemque. Soleat oblique
+        eu ius, ea eum perfecto urbanitas. Homero aliquip cu has, eum cu atqui
+        bonorum vituperata. Adhuc moderatius et nam. Pri ea atomorum urbanitas,
+        eum at autem forensibus. Ut his dolorem tractatos, eam labore feugait
+        singulis ei. Aeque congue splendide cu mei. Ad nec falli primis
+        volutpat, pri paulo labitur in. Vel vidit atqui tibique id, cu mel
+        cetero minimum fastidii. Te vix indoctum laboramus intellegebat. Ea
+        soluta ullamcorper sed, ex essent prompta eos.
+      </div>
     </div>
   );
 }
