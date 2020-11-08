86870b26c8c9adb8f53fce83ec7fe159b785b721
evalynledezma <evalynledezmacode@gmail.com>
2020-11-08 15:04:51 -0700

    Initial Build Out of the Portfolio Detail Component

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,9 +1,44 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import contactPagePicture from "../../../static/assets/images/login.jpg";
 
 export default function () {
   return (
-    <div>
-      <h2>Contact</h2>
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + contactPagePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+
+      <div className="right-column">
+        <div className="contact-bullet-points">
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="phone" />
+            </div>
+
+            <div className="text">801-709-8354</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="envelope" />
+            </div>
+            <div className="text">evalynledezmacode@gmail.com</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="map-marked-alt" />
+            </div>
+            <div className="text">Orem, UT</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
